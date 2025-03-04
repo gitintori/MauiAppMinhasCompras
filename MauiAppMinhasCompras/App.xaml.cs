566c773e78b9ac49d4c58ddfde535ce3a750e591
Giovanna Tintori <giovannatintori@hotmail.com>
2025-03-04 18:41:39 -0300

    alterar resolucao da tela

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -9,5 +9,15 @@
             // MainPage = new AppShell();
             MainPage = new NavigationPage (new Views.ListaProduto());
         }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 750;
+
+            return window;
+        }
     }
 }

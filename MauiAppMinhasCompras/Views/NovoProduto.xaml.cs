b8dac71ee3895a4a50159a3a55617452586ec246
Giovanna Tintori <giovannatintori@hotmail.com>
2025-03-27 22:22:28 -0300

    inserir Navigation.PopAsync para voltar a tela anterior

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -22,7 +22,7 @@ public partial class NovoProduto : ContentPage
 
             await App.Db.Insert(p);
             await DisplayAlert("Sucesso!", "Produto adicionado.", "OK");
-
+            await Navigation.PopAsync(); // serve para retornar a outra tela
         }
         catch (Exception ex)
         {
